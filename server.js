351f86d056b98bd27f5038a5567395a77fe56234
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-12-05 21:54:56 -0500

    checked to see if my backend is working

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 const weatherData = require('./data/weather.json');
@@ -25,47 +25,38 @@ class Forecast {
   }
 }
 
-function processCityWeather(cityName) {
-  const cityWeather = weatherData.find((city) => city.city_name === cityName);
-
-  if (!cityWeather) {
-    return [];
-  }
-
-  const cityForecasts = cityWeather.data.map((day) => {
-    return new Forecast(day.datetime, day.weather.description);
-  });
-
-  return cityForecasts;
-}
-
-
 app.get('/weather', (req, res) => {
   const { lat, lon, searchQuery } = req.query;
 
-  if (lat && lon && searchQuery) {
-    const foundCity = cities.find(
-      (city) =>
-        city.lat === lat &&
-        city.lon === lon &&
-        city.name.toLowerCase() === searchQuery.toLowerCase()
+ 
+  if (lat && lon) {
+    const cityWeather = weatherData.find(
+      (city) => city.lat === lat && city.lon === lon
     );
 
-    if (foundCity) {
-      const cityName = foundCity.name;
-      const cityWeather = weatherData.find(
-        (city) => city.city_name.toLowerCase() === cityName.toLowerCase()
-      );
-
-      if (cityWeather) {
-        res.json(cityWeather.data.map((day) => new Forecast(day.datetime, day.weather.description)));
-      } else {
-        res.status(404).json({ error: 'Weather data not found for the city' });
-      }
+    if (cityWeather) {
+      res.json(cityWeather.data.map((day) => new Forecast(day.datetime, day.weather.description)));
     } else {
-      res.status(404).json({ error: 'City not found' });
+      res.status(404).json({ error: 'Weather data not found for the provided coordinates' });
     }
-  } else {
+  }
+
+  
+  else if (searchQuery) {
+    const cityName = searchQuery.toLowerCase();
+    const cityWeather = weatherData.find(
+      (city) => city.city_name.toLowerCase() === cityName
+    );
+
+    if (cityWeather) {
+      res.json(cityWeather.data.map((day) => new Forecast(day.datetime, day.weather.description)));
+    } else {
+      res.status(404).json({ error: 'Weather data not found for the provided city' });
+    }
+  }
+
+  
+  else {
     res.json(weatherData);
   }
 });
