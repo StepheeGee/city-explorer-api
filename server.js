39f3a326752a71382f663a39b4137b049618b45d
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-12-06 23:02:52 -0500

    removed json logic

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+
 const app = express();
 app.use(cors());
 
@@ -11,11 +12,6 @@ const PORT = process.env.PORT || 3000;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-
-const weatherData = require('./data/weather.json');
-
-
-
 
 class Forecast {
   constructor(date, description) {
@@ -26,7 +22,6 @@ class Forecast {
 
 app.get('/weather', async (req, res) => {
   const { lat, lon, searchQuery } = req.query;
-
 
   if ((lat && lon) || searchQuery) {
     try {
@@ -42,20 +37,28 @@ app.get('/weather', async (req, res) => {
       weatherResponse = await axios.get(apiWeatherUrl);
 
       if (weatherResponse && weatherResponse.data && weatherResponse.data.data) {
-        const forecasts = weatherResponse.data.data.map((day) => {
-          const description = `Low of ${day.low_temp}, high of ${day.high_temp} with ${day.weather.description}`;
-          return { date: day.datetime, description: description };
+        const forecasts = weatherResponse.data.data.map(day => {
+          const forecastDate = new Date(day.datetime);
+          const lowTempFahrenheit = (day.low_temp * 9) / 5 + 32; 
+          const highTempFahrenheit = (day.high_temp * 9) / 5 + 32; 
+    
+          return {
+            description: `Low of ${lowTempFahrenheit.toFixed(1)}°F, high of ${highTempFahrenheit.toFixed(
+              1
+            )}°F with ${day.weather.description}`,
+            date: day.datetime,
+            dayOfWeek: forecastDate.toLocaleString('en-us', { weekday: 'short' }),
+          };
         });
-
         res.json(forecasts);
       } else {
-        res.status(404).json({ error: `We can't find your weather report. Prepare for the worst.` });
+        res.status(404).json({ error: `We can't find your weather forecast. Prepare for the worst.` });
       }
     } catch (error) {
       console.error('Error fetching weather data:', error);
       res.status(500).json({ error: 'Failed to fetch weather data' });
     }
   } else {
-    res.json(weatherData);
+    res.status(400).json({ error: 'Invalid request. Please provide valid latitude and longitude or a search query.' });
   }
 });
